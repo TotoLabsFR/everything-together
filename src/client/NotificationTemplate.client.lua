28987f4849cde1390c4c8058ea98ca07b2911377
TotoCodeFR <totocodefr@duck.com>
2025-09-18 12:35:32 +0200

    adding a boolean to src/client/NotificationTemplate.client.lua

--- src/client/NotificationTemplate.client.lua
+++ src/client/NotificationTemplate.client.lua
@@ -1,10 +1,13 @@
-local NotificationHandler = require(script.Parent.NotificationHandler)
-local CustomListLayout = require(script.Parent.CustomListLayout)
-CustomListLayout.setup(script.Parent.Parent:WaitForChild("App"):WaitForChild("Notifications"))
+local enabled = true
+if enabled then
+    local NotificationHandler = require(script.Parent.NotificationHandler)
+    local CustomListLayout = require(script.Parent.CustomListLayout)
+    CustomListLayout.setup(script.Parent.Parent:WaitForChild("App"):WaitForChild("Notifications"))
 
-local Notification = NotificationHandler.new(15)
-task.wait(3)
-Notification:display()
-task.wait(1)
-local Notification2 = NotificationHandler.new(15)
-Notification2:display()
+    local Notification = NotificationHandler.new(15)
+    task.wait(3)
+    Notification:display()
+    task.wait(1)
+    local Notification2 = NotificationHandler.new(15)
+    Notification2:display()
+end
